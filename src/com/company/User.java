75bb8f245718d373e940103351aec280fed64a12
Dmytro <dmytro.nebelskyi@gmail.com>
2023-05-01 03:30:11 +0300

    New Class added

---
+++ src/com/company/User.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class User {
+
+    int id, age, elo;
+    String name;
+
+}
+
+class Main {
+
+    public static void main(String[] args) {
+
+        User user1 = new User();
+        user1.age = 23;
+        user1.elo=2300;
+        user1.id=192890538;
+        user1.name="d1xonella";
+
+    }
+
+}
